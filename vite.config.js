966a12b7b25634d6286c66f08ff546774e3beedc
Christian Uche <christianuche720@gmail.com>
2025-10-16 04:52:16 -0700

    done

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,10 @@ export default defineConfig({
   root: "src/",
 
   build: {
-    outDir: "../dist",
+    outDir: "dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        scripts: resolve(__dirname, "src/js/events.js"),
       },
     },
   },
