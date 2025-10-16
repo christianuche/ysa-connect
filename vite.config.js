442a0fc532bca160ac9f89c4987531ce32155e1b
Christian Uche <christianuche720@gmail.com>
2025-10-16 04:34:34 -0700

    Added script to handle events

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        scripts: resolve(__dirname, "src/js/events.js"),
       },
     },
   },
